a7281122aed53feb8f5d202b649143c657d2e786
Bassam73 <bassamwalid6@gmail.com>
2026-04-11 22:34:23 +0200

    Fix Comment notification

--- src/modules/notifications/notification.service.ts
+++ src/modules/notifications/notification.service.ts
@@ -3,10 +3,6 @@ import INotification, {
   createNotificationDTO,
   EntityType,
 } from "../../types/notification.types";
-import CommentsService from "../comments/comments.service";
-import FollowService from "../follow/follow.service";
-import PostService from "../posts/posts.service";
-import ReactsService from "../reacts/reacts.service";
 import NotificationRepository from "./notification.repo";
 import FirebaseService from "../../core/services/firebase.service";
 import { PushPayload } from "../../core/services/firebase.service";
@@ -18,30 +14,6 @@ export default class NotificationService {
     this.repo = repo;
   }
   async createNotificaiton(data: createNotificationDTO) {
-    let entity: any;
-    switch (data.entityType) {
-      case EntityType.POST:
-        entity = PostService.getInstace().getPostbyId(data.entityID.toString());
-        break;
-      case EntityType.COMMENT:
-        entity = CommentsService.getInstance().getCommentByID(
-          data.entityID.toString(),
-        );
-        break;
-      case EntityType.FOLLOW:
-        entity = FollowService.getInstance().getFollowByID(
-          data.entityID.toString(),
-        );
-        break;
-      case EntityType.REACT:
-        entity = ReactsService.getInstance().getReactionById(
-          data.entityID.toString(),
-        );
-        break;
-    }
-    if (!entity)
-      throw new AppError("Entity Not Found Please Check EntityID", 404);
-
     const saved = await this.repo.createNotification(data);
 
     this.sendPush(saved).catch((err) =>
